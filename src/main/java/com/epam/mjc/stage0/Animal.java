1a0948694b13be1fabdcf24f37a2e83bc7f9028d
Siarhei_Shpakouski <siarhei_shpakouski@epam.com>
2023-05-09 22:41:19 +0400

    done

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,31 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("This animal is mostly ").append(color)
+                .append(". It has ").append(numberOfPaws);
+        if (numberOfPaws > 1) {
+            sb.append(" paws and ");
+        } else {
+            sb.append(" paw and ");
+        }
+        if (hasFur) {
+            sb.append("a");
+        } else {
+            sb.append("no");
+        }
+        sb.append(" fur.");
+        return sb.toString();
+    }
 }
